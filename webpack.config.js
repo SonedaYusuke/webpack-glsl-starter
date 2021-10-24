15411b7f7c4a271af9c5acd77b45d7d3584e3aa8
SonedaYusuke <yusuke.soneda@gmail.com>
2021-10-24 22:55:41 +0900

    トレイリングスラッシュっぽい感じになった

--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,12 @@ const webpackConfig = {
 
 glob.sync("*.js", {cwd: "src/js"}).forEach((jsName) => {
 	webpackConfig.entry[jsName] = path.resolve("src", JS_DIR, jsName);
+	const dirName = path.basename(jsName, ".js");
 	const tplName = path.basename(jsName, ".js") + ".html";
 	webpackConfig.plugins.push(
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, "src", HTML_DIR, tplName),
-			filename: tplName,
+			filename: dirName === "index" ? "index.html" : dirName + "/index.html",
 			inject: "body",
 			includeSiblingChunks: true,
 			chunks: ["vendor.js", jsName],
